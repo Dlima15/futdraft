37be93618fc08a64e5d6c283e1f1b6e1c4b49200
Dlima15 <danilo.lima7456@gmail.com>
2026-04-19 16:09:17 -0300

    aleatorio

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,20 +9,61 @@ console.log("Js aplicado")
 const configPartida = {
     tipoSorteio: "aleatorio",
     jogadores: [],
+    nivel:[],
     estrutura: [],
     totalTime: [],
     tempo: [],
     criterio: []
 }
 
-// selecionando valor do sorteio:
+let listaJogadores = [
+
+]
+
+// selecionando o tipo do sorteio e ja bloqueando caso o sorteio escolhido for aleatorio
 
 const sorteio = document.getElementById("seletor");
 
 sorteio.addEventListener('change', (event) =>{
-
     const opcaovalor = event.target.value;
+    const containerNivel = document.getElementById('nivelJogador');
 
-    console.log("sorteio escolhido " + opcaovalor)
+    if (opcaovalor == 'aleatorio'){
+        containerNivel.style.opacity ='0.5';
+        containerNivel.style.pointerEvents = "none";
+    } else{
+        containerNivel.style.opacity = '1'
+        containerNivel.style.pointerEvents = "auto";
+    }
+
+    console.log('Modo de sorteio: ' + opcaovalor)
 });
+
+// selecionando os jogadores
+
+const botaoAdicionar = document.querySelector('.botao-inserir');
+
+botaoAdicionar.addEventListener('click', () => {
+    const nomeCapturado =  document.getElementById('nomeJogador').value;
+
+    if (nomeCapturado == ""){
+        alert('Por favor digite o nome do jogador')
+        return;
+    }
+    console.log('Novo jogador: ' + nomeCapturado )
+
+});
+
+// selecionando o nivel do jogador 
+
+const escolhaNivel = document.querySelector('.escolha-nivel');
+
+escolhaNivel.addEventListener('change', (event) =>{
+    const nivelSelecionado = event.target.value;
+
+    console.log("Nivel do jogador: " + nivelSelecionado);
+})
+
+
+
 
