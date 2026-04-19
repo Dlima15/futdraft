bd01b90e5567a15722777c6f61a4352ad60de06b
Dlima15 <danilo.lima7456@gmail.com>
2026-04-18 21:54:33 -0300

    guardando valores da configura;'ao da partida

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
 // Main JS file
+
+console.log("Js aplicado")
+
+const configPartida = {
+    tipoSorteio: "aleatorio",
+    jogadores: [],
+    estrutura: [],
+    totalTime: [],
+    tempo: [],
+    criterio: []
+}
+
