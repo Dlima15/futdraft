d9cb1523c991099b839de617fdb16c00e097542f
Dlima15 <danilo.lima7456@gmail.com>
2026-04-29 13:16:10 -0300

    consertando estrutura geral do codigo que estava errado eexibindo pelo console so pra testar

--- assets/js/sorteio.js
+++ assets/js/sorteio.js
@@ -4,12 +4,17 @@ const dadosMemoria = localStorage.getItem('dadosPartida');
 
 if (dadosMemoria){
     const config = JSON.parse(dadosMemoria);
-    const totalTimes = JSON.parseInt(config.totalTime);
+    const totalTimes = parseInt(config.totalTime);
     const limitePorTime = parseInt(config.estrutura);
-}
 
 let times = Array.from({length: totalTimes}, () => []);
 
+console.log(times);
+
 let lista = [...config.jogadores];
 
+console.log(lista);
+
+
+}
 
