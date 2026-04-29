715e44643c011dd24a7fc9c6dfbfeac65d8e9389
Dlima15 <danilo.lima7456@gmail.com>
2026-04-29 12:57:48 -0300

    aqui buscaos os dados do proprio local storagem e criamos uma lista [] para cada time, além de pegar todos os jogadores salvos no local e colocar eles em uma lista

--- assets/js/sorteio.js
+++ assets/js/sorteio.js
@@ -1,2 +1,15 @@
 console.log('Sorteio Js aplicado!');
 
+const dadosMemoria = localStorage.getItem('dadosPartida');
+
+if (dadosMemoria){
+    const config = JSON.parse(dadosMemoria);
+    const totalTimes = JSON.parseInt(config.totalTime);
+    const limitePorTime = parseInt(config.estrutura);
+}
+
+let times = Array.from({length: totalTimes}, () => []);
+
+let lista = [...config.jogadores];
+
+
