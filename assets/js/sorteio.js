0549fc8ec1dccc92c740ccf2b9087c8dbe468b98
Dlima15 <danilo.lima7456@gmail.com>
2026-05-04 22:32:20 -0300

    sorteando as prieiras partidas e quem espera, e aplicando ao css ja tbm

--- assets/js/sorteio.js
+++ assets/js/sorteio.js
@@ -128,9 +128,58 @@ const btnSortearAdv = document.querySelector(".botao-secundario-sorteio");
 
 btnSortearAdv.addEventListener("click", (event) => {
 
-    const dadosPartida = localStorage.getItem("resultadoSorteio");
-    const jogoSorteio =  JSON.parse(dadosPartida);
-    console.log(jogoSorteio);
+    const storage = localStorage.getItem("resultadoSorteio");
+
+    if (!storage){
+        console.error("Erro: Não encontrei nada no local storage com o nome ResultadoSOrteio");
+        return
+    }
+
+    const timesCompletos = JSON.parse(storage);
+
+    if (!Array.isArray(timesCompletos)){
+        console.error("Erro: Os dados do storage não são uma lista válida.");
+        return
+    }
+
+    const nomeDosTimes = timesCompletos.map(time => {
+        if (time && time.nomeTime) {
+            return time.nomeTime;
+        }
+        return "Time sem nome";
+    });
+
+    const nomesSorteados = [...nomeDosTimes].sort(() => Math.random() - 0.5);
+
+    const time1 = nomesSorteados[0];
+    const time2 = nomesSorteados[1];
+    const espera = nomesSorteados.slice(2); 
+
+    // APLICANDO ISSO AO HTML
+
+    const nomesNoHTML = document.querySelectorAll(".time-item strong");
+    const siglasNoHTML = document.querySelectorAll(".sigla");
+    const textoEpera =  document.querySelector(".fora-da-rodada p");
+
+    nomesNoHTML[0].innerText = time1;
+    nomesNoHTML[1].innerText = time2;
+
+    if (textoEpera) {
+        textoEpera.innerText = espera.join(", ")
+    }
+
+    siglasNoHTML[0].innerText = time1.split(" ").pop().substring(0, 2).toUpperCase();
+    siglasNoHTML[1].innerText = time2.split(" ").pop().substring(0, 2).toUpperCase();
+
+    const corTime1 = time1.split(" ").pop().toLowerCase();
+    const corTime2 = time2.split(" ").pop().toLowerCase();
+
+    siglasNoHTML[0].className = `sigla ${corTime1}`;
+    siglasNoHTML[1].className = `sigla ${corTime2}`;
+
+
+    console.log("Times para o jogo: ", nomesSorteados[0], "Vs", nomesSorteados [1], "e em espera", espera);
+
 
 });
 
